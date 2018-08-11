7eb2f45bfed75193837b4137e0aa302fe4f535c7
zhouzhoujin <772504702@qq.com>
2018-08-11 15:43:29 +0800

    learnvuecli3

---
+++ learnVuecli/src/router.js
@@ -0,0 +1,33 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+import mine from './views/mine.vue';
+
+
+Vue.use(Router);
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+    },
+      {
+        path:'/mine',
+          name:'mine',
+          // component:mine,
+          component: ()=> import('./views/mine.vue')
+      }
+  ],
+});
